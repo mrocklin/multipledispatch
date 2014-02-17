6882990068aaccaa8cf5d3bc2ec0e987e65294cd
Matthew Rocklin <mrocklin@gmail.com>
2014-02-16 20:34:35 -0800

    handle competing multiple dispatch situations

--- multipledispatch.py
+++ multipledispatch.py
@@ -1,5 +1,6 @@
 from contextlib import contextmanager
 
+
 class Dispatcher(object):
     __slots__ = 'name', 'funcs'
     def __init__(self, name):
@@ -13,13 +14,27 @@ class Dispatcher(object):
         types = tuple(map(type, args))
         if types in self.funcs:
             return self.funcs[types]
-        matches = []
+
+        matches = dict()
         n = len(types)
         for signature, func in self.funcs.items():
             if len(signature) == n and all(map(issubclass, types, signature)):
-                matches.append(func)
+                matches[signature] = func
         if len(matches) == 1:
-            return matches[0]
+            return matches.values()[0]
+        if len(matches) > 1:
+            scores = {func: [typ.mro().index(sig)
+                                for typ, sig in zip(types, signature)]
+                            for signature, func in matches.items()}
+            winners = [min([(func, indices) for (func, indices)
+                                            in scores.items()],
+                           key=lambda (f, ind): ind[i])
+                       for i in range(len(types))]
+            winners = set([func for func, ind in winners])
+            if len(winners) != 1:
+                raise Warning("Multiple competing implementations found",
+                              "Using one at random.")
+            return next(iter(winners))
         raise NotImplementedError()
 
     def __call__(self, *args, **kwargs):
@@ -29,6 +44,7 @@ class Dispatcher(object):
 dispatchers = dict()
 
 def dispatch(*types):
+    types = tuple(types)
     def _(func):
         name = func.func_name
         if name not in dispatchers:
--- test_multipledispatch.py
+++ test_multipledispatch.py
@@ -6,11 +6,16 @@ def test_singledispatch():
     def f(x):
         return x + 1
 
+    @dispatch(int)
+    def g(x):
+        return x + 2
+
     @dispatch(float)
     def f(x):
         return x - 1
 
     assert f(1) == 2
+    assert g(1) == 3
     assert f(1.0) == 0
 
 
@@ -30,6 +35,8 @@ def test_multipledispatch():
 class A(object): pass
 class B(object): pass
 class C(A): pass
+class D(C): pass
+class E(C): pass
 
 
 def test_inheritance():
@@ -60,3 +67,41 @@ def test_inheritance_and_multiple_dispatch():
     assert f(A(), B()) == 0
     assert f(C(), B()) == 0
     assert raises(NotImplementedError, lambda: f(B(), B()))
+
+
+def test_competing_solutions():
+    @dispatch(A)
+    def h(x):
+        return 1
+
+    @dispatch(C)
+    def h(x):
+        return 2
+
+    print h(D())
+    assert h(D()) == 2
+
+
+def test_competing_multiple():
+    @dispatch(A, B)
+    def h(x, y):
+        return 1
+
+    @dispatch(C, B)
+    def h(x, y):
+        return 2
+
+    assert h(D(), B()) == 2
+
+
+def test_competing_ambiguous():
+    @dispatch(A, C)
+    def f(x, y):
+        return 2
+
+    @dispatch(C, A)
+    def f(x, y):
+        return 2
+
+    assert f(A(), C()) == f(C(), A()) == 2
+    # assert raises(Warning, lambda : f(A(), C()))
