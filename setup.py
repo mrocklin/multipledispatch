e8f7b7c1bec19e16421506dcb92e190d6f6936d2
Matthew Rocklin <mrocklin@gmail.com>
2014-02-18 18:08:36 -0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+import multipledispatch
+
+setup(name='multipledispatch',
+      version=multipledispatch.__version__,
+      description='Multiple dispatch'
+      url='http://github.com/mrocklin/multipledispatch/',
+      author='Matthew Rocklin',
+      author_email='mrocklin@gmail.com',
+      license='BSD',
+      keywords='dispatch',
+      packages=['multipledispatch']
+      long_description=(open('README.me').read() if exists('README.md')
+                        else ''),
+      zip_safe=False)
