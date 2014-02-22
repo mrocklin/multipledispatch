d3fa70ba4b6837494de70d91a71c85bda02c9b39
Matthew Rocklin <mrocklin@gmail.com>
2014-02-22 08:54:06 -0800

    add super signature function

--- multipledispatch/conflict.py
+++ multipledispatch/conflict.py
@@ -6,3 +6,11 @@ def remove_obsolete(signatures):
     return [a for a in signatures
               if not any(len(a) == len(b) and supercedes(b, a)
                          for b in signatures if b is not a)]
+
+
+def super_signature(signatures):
+    n = len(signatures[0])
+    assert all(len(s) == n for s in signatures)
+
+    return [max([sig[i].mro() for sig in signatures], key=len)[0]
+               for i in range(n)]
--- multipledispatch/tests/test_conflict.py
+++ multipledispatch/tests/test_conflict.py
@@ -16,3 +16,10 @@ def test_remove_obsolete():
     expected = [[B], [A, B], [B, A]]
     result = remove_obsolete(signatures)
     assert set(map(tuple, result)) == set(map(tuple, expected))
+
+
+def test_super_signature():
+    assert super_signature([[A]]) == [A]
+    assert super_signature([[A], [B]]) == [B]
+    assert super_signature([[A, B], [B, A]]) == [B, B]
+    assert super_signature([[A, A, B], [A, B, A], [B, A, A]]) == [B, B, B]
