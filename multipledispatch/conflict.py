3a8ad2cf4d82ef78d4e7baa68cb6f23d7b848050
Matthew Rocklin <mrocklin@gmail.com>
2014-02-22 08:44:40 -0800

    add remove_obsolete function

--- multipledispatch/conflict.py
+++ multipledispatch/conflict.py
@@ -1,2 +1,8 @@
 def supercedes(a, b):
     return len(a) == len(b) and all(map(issubclass, a, b))
+
+
+def remove_obsolete(signatures):
+    return [a for a in signatures
+              if not any(len(a) == len(b) and supercedes(b, a)
+                         for b in signatures if b is not a)]
--- multipledispatch/tests/test_conflict.py
+++ multipledispatch/tests/test_conflict.py
@@ -6,3 +6,13 @@ class C(object): pass
 
 def test_supercedes():
     assert supercedes([B], [A])
+    assert supercedes([B, A], [A, A])
+    assert not supercedes([B, A], [A, B])
+    assert not supercedes([A], [B])
+
+
+def test_remove_obsolete():
+    signatures = [[A], [B], [A, A], [B, A], [A, B]]
+    expected = [[B], [A, B], [B, A]]
+    result = remove_obsolete(signatures)
+    assert set(map(tuple, result)) == set(map(tuple, expected))
