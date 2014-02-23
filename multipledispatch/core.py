33ed2767f36445f6939b1e7a70ba49d694d50c75
Matthew Rocklin <mrocklin@gmail.com>
2014-02-23 08:18:40 -0800

    docstring for str_signature

--- multipledispatch/core.py
+++ multipledispatch/core.py
@@ -132,6 +132,11 @@ def dispatch(*types):
 
 
 def str_signature(sig):
+    """ String representation of type signature
+
+    >>> str_signature((int, float))
+    'int, float'
+    """
     return ', '.join(cls.__name__ for cls in sig)
 
 
