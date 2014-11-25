7ec01b9fcf7cc40c6804a364816eb14b7729a51e
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-11-24 22:52:03 -0600

    Removed 8 tests that were redundant.
    The remaining tests are also a bit redundant, however:
    * a multiple dispatch for function remains as a basic usage sanity check, and
    * there had been early concerns over differing behaviour between methods and
      functions in Python 2/3, so some tests for annotation-based dispatch remain

--- multipledispatch/tests/test_annotations.py
+++ multipledispatch/tests/test_annotations.py
@@ -2,26 +2,6 @@ from multipledispatch import dispatch
 from multipledispatch.utils import raises
 
 
-def test_singledispatch():
-    @dispatch
-    def f(x: int):
-        return x + 1
-
-    @dispatch
-    def g(x: int):
-        return x + 2
-
-    @dispatch
-    def f(x: float):
-        return x - 1
-
-    assert f(1) == 2
-    assert g(1) == 3
-    assert f(1.0) == 0
-
-    assert raises(NotImplementedError, lambda: f('hello'))
-
-
 def test_multipledispatch():
     @dispatch
     def f(x: int, y: int):
@@ -72,79 +52,6 @@ def test_inheritance_and_multiple_dispatch():
     assert raises(NotImplementedError, lambda: f(B(), B()))
 
 
-def test_competing_solutions():
-    @dispatch
-    def h(x: A):
-        return 1
-
-    @dispatch
-    def h(x: C):
-        return 2
-
-    assert h(D()) == 2
-
-
-def test_competing_multiple():
-    @dispatch
-    def h(x: A, y: B):
-        return 1
-
-    @dispatch
-    def h(x: C, y: B):
-        return 2
-
-    assert h(D(), B()) == 2
-
-
-def test_competing_ambiguous():
-    @dispatch
-    def f(x: A, y: C):
-        return 2
-
-    @dispatch
-    def f(x: C, y: A):
-        return 2
-
-    assert f(A(), C()) == f(C(), A()) == 2
-    # assert raises(Warning, lambda : f(C(), C()))
-
-
-def test_caching_correct_behavior():
-    @dispatch
-    def f(x: A):
-        return 1
-
-    assert f(C()) == 1
-
-    @dispatch
-    def f(x: C):
-        return 2
-
-    assert f(C()) == 2
-
-
-def test_union_types():
-    @dispatch
-    def f(x: (A, C)):
-        return 1
-
-    assert f(A()) == 1
-    assert f(C()) == 1
-
-
-"""
-Fails
-def test_dispatch_on_dispatch():
-    @dispatch(A)
-    @dispatch(C)
-    def q(x):
-        return 1
-
-    assert q(A()) == 1
-    assert q(C()) == 1
-"""
-
-
 def test_methods():
     class Foo(object):
         @dispatch
@@ -164,20 +71,3 @@ def test_methods():
     assert foo.f(1) == 2
     assert foo.f(1.0) == 0.0
     assert foo.g(1) == 4
-
-
-def test_methods_multiple_dispatch():
-    class Foo(object):
-        @dispatch
-        def f(x: A, y: A):
-            return 1
-
-        @dispatch
-        def f(x: A, y: C):
-            return 2
-
-
-    foo = Foo()
-    assert foo.f(A(), A()) == 1
-    assert foo.f(A(), C()) == 2
-    assert foo.f(C(), C()) == 2
