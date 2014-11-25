d8fbfea2bf6237888cac3631ebfd6713808fda66
Duncan M. McGreggor <oubiwann@cogitat.io>
2014-11-25 00:46:07 -0600

    Separated functionality out for better testing.
    Also: reverted a doctest change to protect Python 2.x tests.

--- multipledispatch/core.py
+++ multipledispatch/core.py
@@ -53,28 +53,19 @@ def dispatch(*types, **kwargs):
     ...     def __init__(self, datum):
     ...         self.data = [datum]
 
-    Example usage for Python 3 as compared with Python 2 would be:
+    Example usage for Python 3 (which can't be entered as doctests,
+    since Python 2 would fail tp parse it):
 
-    >>> import sys
-    >>> if sys.version_info.major >= 3:
-    ...     @dispatch
-    ...     def f(x: int):
-    ...         return x + 3
-    ...     @dispatch
-    ...     def f(x: float):
-    ...         return x + 3.0
-    ... else:
-    ...     @dispatch(int)
-    ...     def f(x):
-    ...         return x + 3
-    ...     @dispatch(float)
-    ...     def f(x):
-    ...         return x + 3.0
+    @dispatch
+    def f(x: int):
+        return x + 3
 
-    >>> f(3)
-    6
-    >>> f(3.0)
-    6.0
+    @dispatch
+    def f(x: float):
+        return x + 3.0
+
+    Calling these functions as f(3) and f(3.0) would return 6 and 6.0,
+    respectively.
     """
     # if one argument as passed that is not callable and isn't a type, dispatch
     # on annotations
@@ -90,8 +81,7 @@ def dispatch(*types, **kwargs):
 
 
 def dispatch_on_types(*types, **kwargs):
-    """
-    Dispatch based on the types that are provided as arguments.
+    """ Dispatch based on the types that are provided as arguments.
     """
     namespace = kwargs.get('namespace', global_namespace)
     on_ambiguity = kwargs.get('on_ambiguity', ambiguity_warn)
@@ -113,15 +103,28 @@ def dispatch_on_types(*types, **kwargs):
     return _
 
 
-def dispatch_on_annotations(fn, **kwargs):
+def get_types_from_annotations(fn):
+    """ This function has been separated out in order to make testing cleaner.
+    It is intended only for use by dispatch_on_annotations.
+
+    This should only ever be called by Python 3.4.
     """
-    Dispatch types that are calculated from from fn's annotation (only works
-    in Python 3+).
+    argspec = inspect.getfullargspec(fn)
+    if ismethod(fn):
+        args = argspec.args[1:]
+    else:
+        args = argspec.args
+    anns = argspec.annotations
+    return [anns.get(a) for a in args if anns.get(a)]
+
+
+def dispatch_on_annotations(fn, **kwargs):
+    """ Dispatch types that are calculated from from fn's annotation.
+
+    This should only ever be called by Python 3.4.
     """
     if sys.version_info.major >= 3:
-        argspec = inspect.getfullargspec(fn)
-        args = argspec.args[1:] if ismethod(fn) else argspec.args
-        types = [argspec.annotations[a] for a in args]
+        types = get_types_from_annotations(fn)
         return dispatch_on_types(*types, **kwargs)(fn)
     else:
         raise SyntaxError('Annotations require Python 3+.')
