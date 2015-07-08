a8d7a8d86f27e6e08054e97dde1f87354c8d621a
Matthew Rocklin <mrocklin@gmail.com>
2015-07-08 23:00:44 +0200

    skip order dependent groupby doctests

--- multipledispatch/utils.py
+++ multipledispatch/utils.py
@@ -88,11 +88,11 @@ def groupby(func, seq):
     """ Group a collection by a key function
 
     >>> names = ['Alice', 'Bob', 'Charlie', 'Dan', 'Edith', 'Frank']
-    >>> groupby(len, names)
+    >>> groupby(len, names)  # doctest: +SKIP
     {3: ['Bob', 'Dan'], 5: ['Alice', 'Edith', 'Frank'], 7: ['Charlie']}
 
     >>> iseven = lambda x: x % 2 == 0
-    >>> groupby(iseven, [1, 2, 3, 4, 5, 6, 7, 8])
+    >>> groupby(iseven, [1, 2, 3, 4, 5, 6, 7, 8])  # doctest: +SKIP
     {False: [1, 3, 5, 7], True: [2, 4, 6, 8]}
 
     See Also:
