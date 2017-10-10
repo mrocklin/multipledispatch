029425f1002ca556fc2086838ff4882c1c750c26
Phillip Cloud <cpcloud@gmail.com>
2017-10-10 11:57:24 -0400

    Make bench/test_simple.py py3 compatible

--- bench/test_simple.py
+++ bench/test_simple.py
@@ -1,14 +1,22 @@
 from multipledispatch import dispatch
 
+try:
+    range = xrange
+except NameError:
+    pass
+
+
 @dispatch(int)
 def isint(x):
     return True
+
 
 @dispatch(object)
 def isint(x):
     return False
 
+
 def test_simple():
-    for i in xrange(100000):
-        isint(5)
-        isint('a')
+    for i in range(100000):
+        assert isint(5)
+        assert not isint('a')
